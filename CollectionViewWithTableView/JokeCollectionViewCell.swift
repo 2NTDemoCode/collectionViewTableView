3743edc9ebd0366dca924ff270064cfea1964b1a
Minea Chem <minea@mineas-Mac.local>
2017-04-04 10:50:41 +0700

    frist commit

---
+++ CollectionViewWithTableView/JokeCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  JokeCollectionViewCell.swift
+//  CollectionViewWithTableView
+//
+//  Created by Minea Chem on 3/23/17.
+//  Copyright © 2017 Minea Chem. All rights reserved.
+//
+
+import UIKit
+
+class JokeCollectionViewCell: UICollectionViewCell {
+    
+}
